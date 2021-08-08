e949aa8a6ed6458dcedc07f1b27cda822e2d7c18
Djumabaevs <djumabaevb@gmail.com>
2021-08-08 10:53:41 +0600

    live data post values

--- app/src/main/java/com/djumabaevs/movieswatch/MovieRepository.kt
+++ app/src/main/java/com/djumabaevs/movieswatch/MovieRepository.kt
@@ -17,5 +17,13 @@ class MovieRepository(private val movieService: MovieService) {
     get() = errorLiveData
 
     private val apiKey = "89691d3075f96e7c7e086344e47cfb18"
-    fun fetchMovies() = movieService.getPopularMovies(apiKey)
+//    fun fetchMovies() = movieService.getPopularMovies(apiKey)
+    suspend fun fetchMovies() {
+        try {
+            val popularMovies = movieService.getPopularMovies(apiKey)
+            movieLiveData.postValue(popularMovies.results)
+        } catch (e: Exception) {
+            errorLiveData.postValue("Error occured: ${e.message}")
+        }
+    }
 }
