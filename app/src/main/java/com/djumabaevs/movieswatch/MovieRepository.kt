38908780a0918cdd43fd336d1e4e0a1efbab5c03
Djumabaevs <djumabaevb@gmail.com>
2021-08-08 10:49:02 +0600

    movie and error mutable live data

--- app/src/main/java/com/djumabaevs/movieswatch/MovieRepository.kt
+++ app/src/main/java/com/djumabaevs/movieswatch/MovieRepository.kt
@@ -1,8 +1,13 @@
 package com.djumabaevs.movieswatch
 
+import androidx.lifecycle.MutableLiveData
 import com.djumabaevs.movieswatch.api.MovieService
+import com.djumabaevs.movieswatch.model.Movie
 
 class MovieRepository(private val movieService: MovieService) {
+
+    private val movieLiveData = MutableLiveData<List<Movie>>()
+    private val errorLiveData = MutableLiveData<String>()
 
     private val apiKey = "89691d3075f96e7c7e086344e47cfb18"
     fun fetchMovies() = movieService.getPopularMovies(apiKey)
